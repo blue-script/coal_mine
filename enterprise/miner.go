69976e80a096cf326bbd74653996f5806c82ef15
blue-script <alexm92@inbox.ru>
2026-03-15 13:48:32 +0500

    refactor: optimize miner concurrency with RWMutex and improve initialization

--- enterprise/miner.go
+++ enterprise/miner.go
@@ -35,7 +35,7 @@ type previewFunc func() (Coal, bool)
 type commitFunc func(coal Coal)
 
 type baseMiner struct {
-	mtx                  sync.Mutex
+	mtx                  sync.RWMutex
 	Class                MinerClass
 	HireCost             int
 	Energy               int
@@ -67,8 +67,8 @@ type StrongMiner struct {
 func (m *baseMiner) startRun() (chan Coal, bool) {
 	ch := make(chan Coal)
 
-	defer m.mtx.Unlock()
 	m.mtx.Lock()
+	defer m.mtx.Unlock()
 	if m.Working {
 		close(ch)
 		return ch, false
@@ -86,8 +86,8 @@ func (m *baseMiner) finishRun(ch chan Coal) {
 }
 
 func (m *baseMiner) defaultPreview() (Coal, bool) {
-	m.mtx.Lock()
-	defer m.mtx.Unlock()
+	m.mtx.RLock()
+	defer m.mtx.RUnlock()
 
 	if m.RemainingEnergy == 0 {
 		return 0, false
@@ -123,21 +123,20 @@ func (m *baseMiner) Run(ctx context.Context) <-chan Coal {
 		ticker := time.NewTicker(m.Delay)
 		defer ticker.Stop()
 
-	Loop:
 		for {
 			select {
 			case <-ctx.Done():
-				break Loop
+				return
 			case <-ticker.C:
 				coal, ok := m.preview()
 				if !ok {
-					break Loop
+					return
 				}
 
 				select {
 				case <-ctx.Done():
-					break Loop
-				case ch <- Coal(coal):
+					return
+				case ch <- coal:
 					m.commit(coal)
 				}
 			}
@@ -148,8 +147,8 @@ func (m *baseMiner) Run(ctx context.Context) <-chan Coal {
 }
 
 func (m *baseMiner) Info() MinerInfo {
-	m.mtx.Lock()
-	defer m.mtx.Unlock()
+	m.mtx.RLock()
+	defer m.mtx.RUnlock()
 
 	return MinerInfo{
 		Class:              m.Class,
@@ -162,3 +161,63 @@ func (m *baseMiner) Info() MinerInfo {
 	}
 }
 
+type MinerConfig struct {
+	Class                MinerClass
+	HireCost             int
+	Energy               int
+	CoalPerMine          Coal
+	Delay                time.Duration
+	PerformanceIncrement Coal
+}
+
+func newBaseMiner(cfg MinerConfig) *baseMiner {
+	bm := &baseMiner{
+		Class:                cfg.Class,
+		HireCost:             cfg.HireCost,
+		Energy:               cfg.Energy,
+		CoalPerMine:          cfg.CoalPerMine,
+		Delay:                cfg.Delay,
+		PerformanceIncrement: cfg.PerformanceIncrement,
+	}
+	bm.RemainingEnergy = bm.Energy
+	bm.CurrentCoalPerMine = bm.CoalPerMine
+
+	bm.preview = bm.defaultPreview
+	bm.commit = bm.defaultCommit
+
+	return bm
+}
+
+func NewSmallMiner() *SmallMiner {
+	return &SmallMiner{newBaseMiner(MinerConfig{
+		Class:                MinerClassSmall,
+		HireCost:             5,
+		Energy:               30,
+		CoalPerMine:          1,
+		Delay:                3 * time.Second,
+	})}
+}
+
+func NewNormalMiner() *NormalMiner {
+	return &NormalMiner{newBaseMiner(MinerConfig{
+		Class:                MinerClassNormal,
+		HireCost:             50,
+		Energy:               45,
+		CoalPerMine:          3,
+		Delay:                2 * time.Second,
+	})}
+}
+
+func NewStrongMiner() *StrongMiner {
+	sm := &StrongMiner{newBaseMiner(MinerConfig{
+		Class:                MinerClassStrong,
+		HireCost:             450,
+		Energy:               60,
+		CoalPerMine:          10,
+		Delay:                1 * time.Second,
+		PerformanceIncrement: 3,
+	})}
+	sm.commit = sm.strongCommit
+
+	return sm
+}
